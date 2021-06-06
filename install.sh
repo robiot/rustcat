2d14a614ccb15cef660b32a1f5198632cf2b02be
robi0t <elliot.lindberg07@gmail.com>
2021-06-06 11:49:41 +0300

    📥 Easier Build & Install | Gitignore fix

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 rustcat="./target/release/rc"
 usrbin="/usr/bin/rc"
 
-echo "Copying" $rustcat "To" $usrbin
-cp $rustcat $usrbin
+
+cargo build --release
 if [ $? -eq 0 ]; then
     echo "Completed"
 else
-    echo "Failed, Remember to run with sudo"
+    echo "Failed to build. Dont run this as sudo"
+    exit 1
+fi
+
+echo "Copying" $rustcat "To" $usrbin
+sudo cp $rustcat $usrbin
+if [ $? -eq 0 ]; then
+    echo "Completed | run with 'rc'"
+else
+    echo "Failed"
     exit 1
 fi
