72a1cbf345fdc631657af715551cd5cb438bd2db
robiot <r0w80abc@gmail.com>
2022-01-01 19:45:56 +0200

    Fix command history on Local mode

--- src/listener/mod.rs
+++ src/listener/mod.rs
@@ -139,8 +139,8 @@ pub fn listen(opts: &utils::Opts) -> Result<()> {
 /* readline_decorator takes in a function, A mutable closure
  * which will perform the sending of data depending on the transport protocol. */
 fn readline_decorator(mut f: impl FnMut(String)) -> Result<()> {
+    let mut rl = Editor::<()>::new();
     loop {
-        let mut rl = Editor::<()>::new();
         match rl.readline(">> ") {
             Ok(command) => {
                 rl.add_history_entry(command.clone().as_str());
